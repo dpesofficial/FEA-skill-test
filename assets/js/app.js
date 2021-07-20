e33cddc7b5220b43c020526263bd96870631d286
Dipesh Thapa <dipesh@naphix.com.au>
2021-07-20 14:03:28 +0545

    Git Initial Upload

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+import jQuery from 'jquery';
+window.$ = jQuery;
+import "@popperjs/core";
+import "bootstrap";
+import "slick-carousel";
+
+var FeaSkillTest = {
+    test: function(){
+        
+    }
+}
+
+FeaSkillTest.test();
